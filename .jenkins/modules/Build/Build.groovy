37d172543715ba60798a74ddfd497039070c49de
unknown <H26A11920@kccs.auth>
2022-10-24 14:45:46 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,29 +1,29 @@
-parallel {
+def modules = [:]
+modules["sub-module1"] = {
     stage("sub-module1"){
         node("sub-module1-node"){
             steps{
-                    MPLModule('Maven Build', [
-                        maven.module_name= "sub-module1"
-                    ])
-            }
-        }
-    }
-     stage("sub-module1"){
-        node("sub-module2-node"){
-            steps{
-                    MPLModule('Maven Build', [
-                        maven.module_name= "sub-module2"
-                    ])
-            }
-        }
-    }
-    stage("module-2"){
-        node("module-2-node"){
-            steps{
-                    MPLModule('Maven Build', [
-                        maven.module_name= "module-2"
-                    ])
+                    bat 'mvn install -pl module-1/sub-module1 -DskipTests'
             }
         }
     }
 }
+modules["sub-module2"] = {
+     stage("sub-module1"){
+        node("sub-module2-node"){
+            steps{
+                    bat 'mvn install -pl module-1/sub-module2 -DskipTests'
+            }
+        }
+    }
+}
+modules["module-2"]= {
+    stage("module-2"){
+        node("module-2-node"){
+            steps{
+                    bat 'mvn install -pl module-2 -DskipTests'
+            }
+        }
+    }
+}
+parallel modules
