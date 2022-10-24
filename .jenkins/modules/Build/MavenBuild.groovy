4e1055d814b9030ea3b19c612e02fa8cdab63db0
unknown <H26A11920@kccs.auth>
2022-10-24 15:22:59 +0900

    update

--- .jenkins/modules/Build/MavenBuild.groovy
+++ .jenkins/modules/Build/MavenBuild.groovy
@@ -1,4 +1,4 @@
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  bat """mvn install -pl '${CFG.'maven.module_name'}'"""
+  bat """mvn install -pl '${CFG.'module_name'}'"""
 }
