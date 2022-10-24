729059ea2a955f25196b58e22a72354d30808da3
unknown <H26A11920@kccs.auth>
2022-10-24 15:15:05 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,20 +1,23 @@
-def modules = [:]
-modules["sub-module1"] = {
-    stage("sub-module1"){
-        node("sub-module1-node"){
-            steps{
-                    echo 'test'
+stage("Build 2"){
+    def modules = [:]
+    modules["sub-module1"] = {
+        stage("sub-module1"){
+            node("sub-module1-node"){
+                steps{
+                        echo 'test'
+                }
             }
         }
     }
-}
-modules["sub-module2"] = {
-     stage("sub-module1"){
-        node("sub-module2-node"){
-            steps{
-                    echo 'test'
+    modules["sub-module2"] = {
+        stage("sub-module1"){
+            node("sub-module2-node"){
+                steps{
+                        echo 'test'
+                }
             }
         }
     }
+    parallel (modules)
+
 }
-parallel (modules)
