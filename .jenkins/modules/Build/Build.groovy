706cefb0d8c75bb47e1c3937bcf340beac182a8c
unknown <H26A11920@kccs.auth>
2022-10-24 14:50:01 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,29 +1,23 @@
 def modules = [:]
 modules["sub-module1"] = {
     stage("sub-module1"){
-        node("sub-module1-node"){
-            steps{
-                    bat 'mvn install -pl module-1/sub-module1 -DskipTests'
-            }
+        steps{
+            bat 'mvn install -pl module-1/sub-module1 -DskipTests'
         }
     }
 }
 modules["sub-module2"] = {
      stage("sub-module1"){
-        node("sub-module2-node"){
-            steps{
-                    bat 'mvn install -pl module-1/sub-module2 -DskipTests'
-            }
+        steps{
+            bat 'mvn install -pl module-1/sub-module2 -DskipTests'
         }
     }
 }
 modules["module-2"]= {
     stage("module-2"){
-        node("module-2-node"){
-            steps{
-                    bat 'mvn install -pl module-2 -DskipTests'
-            }
+        steps{
+            bat 'mvn install -pl module-2 -DskipTests'
         }
     }
 }
-parallel modules
+parallel (modules)
