109da57b8cb4a84440006ba9d40a122a16874b66
unknown <H26A11920@kccs.auth>
2022-10-24 13:59:22 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,1 +1,17 @@
-MPLModule('Maven Build', CFG)
+def modules = [:]
+modules["sub-module1"] = {
+    steps{
+        MPLModule('Maven Build', CFG)
+    }
+}
+modules["sub-module2"] = {
+    steps{
+        MPLModule('Maven Build', CFG)
+    }
+}
+modules["module-2"]= {
+    steps{
+        MPLModule('Maven Build', CFG)
+    }
+}
+parallel modules
