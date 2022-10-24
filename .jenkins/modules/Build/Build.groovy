c507144fbe008bdcf8aca2efa24181df65736734
unknown <H26A11920@kccs.auth>
2022-10-24 15:21:51 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,15 +1,23 @@
-stage("Build 2"){
-    def modules = [:]
-    modules["sub-module1"] = {
-        stage("sub-module1"){
-            echo 'test'
-        }
+def modules = [:]
+modules["sub-module1"] = {
+    stage("sub-module1"){
+        MPLModule("Maven Build",[
+            maven.module_name="module-1/sub-module1"
+        ])
     }
-    modules["sub-module2"] = {
-        stage("sub-module1"){
-            echo 'test'
-        }
-    }
-    parallel (modules)
-
 }
+modules["sub-module2"] = {
+    stage("sub-module2"){
+        MPLModule("Maven Build",[
+            maven.module_name="module-1/sub-module2"
+        ])
+    }
+}
+modules["module-2"] = {
+    stage("module-2"){
+        MPLModule("Maven Build",[
+            maven.module_name="module-2"
+        ])
+    }
+}
+parallel (modules)
