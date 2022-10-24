ca4847c344ad66af34659e2268942f5f85845c29
unknown <H26A11920@kccs.auth>
2022-10-24 15:01:25 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -3,7 +3,7 @@ modules["sub-module1"] = {
     stage("sub-module1"){
         node("sub-module1-node"){
             steps{
-                    bat 'mvn clean install -pl module-1/sub-module1 -DskipTests'
+                    bat 'echo test'
             }
         }
     }
@@ -12,7 +12,7 @@ modules["sub-module2"] = {
      stage("sub-module1"){
         node("sub-module2-node"){
             steps{
-                    bat 'mvn clean install -pl module-1/sub-module2 -DskipTests'
+                    bat 'echo test'
             }
         }
     }
@@ -21,7 +21,7 @@ modules["module-2"]= {
     stage("module-2"){
         node("module-2-node"){
             steps{
-                    bat 'mvn clean install -pl module-2 -DskipTests'
+                    bat 'echo test'
             }
         }
     }
