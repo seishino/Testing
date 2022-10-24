69426da8d53441bd49ff76113f9d4f2b39e6caad
unknown <H26A11920@kccs.auth>
2022-10-24 14:11:52 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -3,7 +3,9 @@ modules["sub-module1"] = {
     stage("sub-module1"){
         node("sub-module1-node"){
             steps{
-                    MPLModule('Maven Build', CFG)
+                    MPLModule('Maven Build', [
+                        maven.module_name: "sub-module1"
+                    ])
             }
         }
     }
@@ -12,7 +14,9 @@ modules["sub-module2"] = {
      stage("sub-module1"){
         node("sub-module2-node"){
             steps{
-                    MPLModule('Maven Build', CFG)
+                    MPLModule('Maven Build', [
+                        maven.module_name: "sub-module2"
+                    ])
             }
         }
     }
@@ -21,7 +25,9 @@ modules["module-2"]= {
     stage("module-2"){
         node("module-2-node"){
             steps{
-                    MPLModule('Maven Build', CFG)
+                    MPLModule('Maven Build', [
+                        maven.module_name: "module-2"
+                    ])
             }
         }
     }
--- .jenkins/modules/Build/MavenBuild.groovy
+++ .jenkins/modules/Build/MavenBuild.groovy
@@ -1,4 +1,4 @@
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  bat """mvn install"""
+  bat """mvn install -p '${CFG.'maven.module_name'}'"""
 }
