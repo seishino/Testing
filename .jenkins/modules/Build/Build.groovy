f60083f59493478345cdf2448870c965057a6f05
unknown <H26A11920@kccs.auth>
2022-10-24 15:23:45 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -2,21 +2,21 @@ def modules = [:]
 modules["sub-module1"] = {
     stage("sub-module1"){
         MPLModule("Maven Build",[
-            module_name="module-1/sub-module1"
+            module_name:"module-1/sub-module1"
         ])
     }
 }
 modules["sub-module2"] = {
     stage("sub-module2"){
         MPLModule("Maven Build",[
-            module_name="module-1/sub-module2"
+            module_name:"module-1/sub-module2"
         ])
     }
 }
 modules["module-2"] = {
     stage("module-2"){
         MPLModule("Maven Build",[
-            module_name="module-2"
+            module_name:"module-2"
         ])
     }
 }
