15184aa51b9870b2fb9aa0c4ad7f1014fb4297b7
unknown <H26A11920@kccs.auth>
2022-10-25 14:21:01 +0900

    update

--- .jenkins2/modules/Build/Build.groovy
+++ .jenkins2/modules/Build/Build.groovy
@@ -1,13 +1,14 @@
-evaluate(new File(".jenkins/modules/Common.groovy"))
+// evaluate(new File(".jenkins/modules/Common.groovy"))
 
-def modules = [:]
-MODULES_LIST.each{
-    modules[it.stage_title] = {
-        stage(it.stage_title){
-            MPLModule("Maven Build",[
-                module_name: it.module_name
-            ])
-        }
-    }
-}
-parallel (modules)
+// def modules = [:]
+// MODULES_LIST.each{
+//     modules[it.stage_title] = {
+//         stage(it.stage_title){
+//             MPLModule("Maven Build",[
+//                 module_name: it.module_name
+//             ])
+//         }
+//     }
+// }
+// parallel (modules)
+bat 'echo test'
