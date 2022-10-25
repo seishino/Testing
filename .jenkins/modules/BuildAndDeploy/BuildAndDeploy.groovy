14cb0337447d136d188d289acee52f2e74be2d35
unknown <H26A11920@kccs.auth>
2022-10-25 16:07:22 +0900

    update

--- .jenkins/modules/BuildAndDeploy/BuildAndDeploy.groovy
+++ .jenkins/modules/BuildAndDeploy/BuildAndDeploy.groovy
@@ -1,6 +1,6 @@
-MPLModule('Common')
+
 def tasks = [:]
-module_list.each{
+CFG.'module_list'.each{
     tasks[it.stage_title] = {
         stage(it.stage_title){
             bat "echo 'build ${it.module_name} module'"
