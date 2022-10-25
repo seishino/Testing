ac9447111446cdeeaaa663559875096bd9f048a7
unknown <H26A11920@kccs.auth>
2022-10-25 11:39:40 +0900

    update

--- .jenkins/modules/Build/Build.groovy
+++ .jenkins/modules/Build/Build.groovy
@@ -1,20 +1,6 @@
-
 def modules = [:]
-def modules_list = [
-    [
-        stage_title: "sub-module1",
-        module_name: "module-1/sub-module1"
-    ],
-    [
-        stage_title: "sub-module2",
-        module_name: "module-1/sub-module1"
-    ],
-    [
-        stage_title: "module-2",
-        module_name: "module-2"
-    ]
-]
-modules_list.each{
+def common = load '../Common.groovy'
+common.modules_list.each{
     modules[it.stage_title] = {
         stage(it.stage_title){
             MPLModule("Maven Build",[
---
+++ .jenkins/modules/Common.groovy
@@ -0,0 +1,15 @@
+
+def modules_list = [
+    [
+        stage_title: "sub-module1",
+        module_name: "module-1/sub-module1"
+    ],
+    [
+        stage_title: "sub-module2",
+        module_name: "module-1/sub-module1"
+    ],
+    [
+        stage_title: "module-2",
+        module_name: "module-2"
+    ]
+]
